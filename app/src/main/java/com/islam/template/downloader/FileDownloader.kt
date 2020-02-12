b018043703cbf8957b91b04de26e6006f5a162a9
islam <islam.m7md.lotfy@gmail.com>
2020-02-12 15:23:24 +0200

    download single image done

---
+++ app/src/main/java/com/islam/template/downloader/FileDownloader.kt
@@ -0,0 +1,35 @@
+package com.islam.template.downloader
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import io.reactivex.Single
+import java.io.File
+import java.io.FileOutputStream
+import java.net.URL
+
+
+class FileDownloader constructor(private val cacheDirectory: String) {
+    fun downloadFile(url: String, fileName: String): Single<Bitmap> {
+        return Single.create { emitter ->
+            val connection = URL(url).openConnection()
+            connection.connect()
+            val inputStream = connection.getInputStream()
+            val fileCache = File(cacheDirectory, fileName)
+            val fileOutputStream = FileOutputStream(fileCache)
+            val buferSize = 16384
+            val buffer = ByteArray(buferSize)
+            do {
+                val numberOfRead = inputStream.read(buffer)
+                if (numberOfRead <= 0) break
+                fileOutputStream.write(buffer, 0, numberOfRead)
+            } while (true)
+            try {
+                emitter.onSuccess(BitmapFactory.decodeFile(fileCache.absolutePath))
+            } catch (e: Exception) {
+                emitter.onError(e)
+            }
+
+        }
+    }
+
+}
--- app/src/main/java/com/islam/template/view/activity/HomeActivity.kt
+++ app/src/main/java/com/islam/template/view/activity/HomeActivity.kt
@@ -1,13 +1,32 @@
 package com.islam.template.view.activity
+
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import com.islam.template.R
+import com.islam.template.downloader.FileDownloader
 import dagger.android.support.DaggerAppCompatActivity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : DaggerAppCompatActivity() {
 
-
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        val fileDownloader = FileDownloader(applicationContext.cacheDir.absolutePath)
+        fileDownloader.downloadFile(
+            url = "https://images.unsplash.com/profile-1464495186405-68089dcd96c3?ixlib=rb-0.3.5\\u0026q=80\\u0026fm=jpg\\u0026crop=faces\\u0026fit=crop\\u0026h=128\\u0026w=128\\u0026s=622a88097cf6661f84cd8942d851d9a2",
+            fileName = "1.png"
+        )
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                iv_home.setImageBitmap(it)
+            }, {
+                Log.e("errrorrrrrrrrr", it.toString())
+            })
     }
 }
